d0b67025230345f9c12984e004ecbde2ea867c8d
Romain Monceau <romain@akeneo.com>
2015-06-05 12:08:47 +0200

    Add DevToolboxBundle

---
+++ PimDevToolboxBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Pim\Bundle\DevToolboxBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Romain Monceau <romain@akeneo.com>
+ */
+class PimDevToolboxBundle extends Bundle
+{
+}
